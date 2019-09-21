88c9d16e3ebd0a0f998b35d9f05ba15f7fcca7be
Benito van der Zander <benito@benibela.de>
2019-09-21 17:53:40 +0200

    ubuntu bionic has four wine packages

--- .travis.install.py
+++ .travis.install.py
@@ -8,7 +8,7 @@ import subprocess
 
 OS_NAME=os.environ.get('TRAVIS_OS_NAME') or 'linux'
 OS_PMAN={'linux': 'sudo apt-get', 'osx': 'brew'}[OS_NAME]
-PKG_WINE={'linux': 'wine-stable', 'osx': 'wine'}[OS_NAME]
+PKG_WINE={'linux': 'wine32', 'osx': 'wine'}[OS_NAME]
 
 LAZ_TMP_DIR=os.environ.get('LAZ_TMP_DIR') or 'lazarus_tmp'
 LAZ_REL_DEF=os.environ.get('LAZ_REL_DEF') or {'linux':'amd64', 'qemu-arm':'amd64', 'qemu-arm-static':'amd64', 'osx':'i386', 'wine':'32'}
